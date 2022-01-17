7e2e5d74a42abb63aa7a5b78fead5abfe5ce0b19
Gabriele <gabriele.maris@gmail.com>
2022-01-17 16:15:24 +0100

    Entry page added, Reduced component scan area

--- MauxKafkaConsumer/src/main/java/it/mauxilium/MauxKafkaConsumer/MauxKafkaConsumerApplication.java
+++ MauxKafkaConsumer/src/main/java/it/mauxilium/MauxKafkaConsumer/MauxKafkaConsumerApplication.java
@@ -2,12 +2,14 @@ package it.mauxilium.MauxKafkaConsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("it.mauxilium.MauxKafkaProducer.framework")
 public class MauxKafkaConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MauxKafkaConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MauxKafkaConsumerApplication.class, args);
+    }
 
 }
